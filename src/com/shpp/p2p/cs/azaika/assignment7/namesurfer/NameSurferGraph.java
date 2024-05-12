a87fdbc24766d2701d9d684284b91a222c4904f2
Lytiy74 <lytiy7404@gmail.com>
2024-05-12 18:11:35 +0300

    Implemented graph drawing

--- src/com/shpp/p2p/cs/azaika/assignment7/namesurfer/NameSurferGraph.java
+++ src/com/shpp/p2p/cs/azaika/assignment7/namesurfer/NameSurferGraph.java
@@ -11,12 +11,15 @@ package com.shpp.p2p.cs.azaika.assignment7.namesurfer;
 
 import acm.graphics.*;
 
+import java.awt.*;
 import java.awt.event.*;
 import java.util.HashMap;
 
 public class NameSurferGraph extends GCanvas
         implements NameSurferConstants, ComponentListener {
     HashMap<String, NameSurferEntry> entryHashMap = new HashMap<>();
+    Color[] colors = {Color.BLACK,Color.BLUE,Color.GREEN,Color.RED};
+    
 
     /**
      * Creates a new NameSurferGraph object that displays the data.
@@ -30,7 +33,7 @@ public class NameSurferGraph extends GCanvas
      * Clears the list of name surfer entries stored inside this class.
      */
     public void clear() {
-        this.removeAll();
+        entryHashMap.clear();
         update();
     }
 	
@@ -56,7 +59,43 @@ public class NameSurferGraph extends GCanvas
      */
     public void update() {
         removeAll();
+        drawEntriesGraphs();
         drawSheet();
+    }
+
+    private void drawEntriesGraphs() {
+        int j = 0;
+        for (NameSurferEntry entry : entryHashMap.values()) {
+            for (int i = 0; i < NDECADES; i++) {
+                int x = getX(i);
+                int y = getY(entry, i);
+                GLabel label = new GLabel(entry.getName() + " " + entry.getRank(i), x, y);
+                label.setColor(colors[j]);
+                add(label);
+
+                GLine line = new GLine(x, y, getX(i - 1), getY(entry, i - 1));
+                line.setColor(colors[j]);
+                add(line);
+            }
+            j++;
+            j = Math.min(j, colors.length - 1);
+        }
+    }
+
+    private int getX(int i) {
+        return Math.max(i,0) * (getWidth() / NDECADES);
+    }
+
+    private int getY(NameSurferEntry entry, int i) {
+        int maxY = getHeight() - GRAPH_MARGIN_SIZE;
+        int y;
+        int rank = entry.getRank(Math.max(i, 0));
+        if (rank == 0){
+            y = maxY;
+        }else if (rank > maxY){
+            y = maxY;
+        }else y = Math.max(rank, GRAPH_MARGIN_SIZE);
+        return y;
     }
 
     private void drawSheet() {
@@ -66,11 +105,11 @@ public class NameSurferGraph extends GCanvas
         add(new GLine(0, getHeight() - GRAPH_MARGIN_SIZE, getWidth(),getHeight() - GRAPH_MARGIN_SIZE));
         for (int i = 0; i < NDECADES; i++) {
             //draw vertical line to separate decades
-            add(new GLine(i * (getWidth()/NDECADES), 0, i * (getWidth()/NDECADES), getHeight()));
+            add(new GLine(getX(i), 0, getX(i), getHeight()));
             int decade = START_DECADE + (10 * i);
             //draw label with number of year
             GLabel label = new GLabel(String.valueOf(decade));
-            label.setLocation(i * (getWidth()/NDECADES),getHeight() - label.getDescent());
+            label.setLocation(getX(i),getHeight() - label.getDescent());
             add(label);
         }
     }
